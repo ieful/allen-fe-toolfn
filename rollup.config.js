29a1fcd3d6fddfacd57cb203c32d904f5e6a8195
xiaoyu <gs1521adc@buaa.edu.cn>
2023-07-22 13:14:32 +0800

    feat:一次生成多种格式

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,41 @@ import typescript from '@rollup/plugin-typescript';
 
 export default {
     input: 'src/index.ts',
-    output: {
-        file: 'dist/bundle.js',
-        format: 'cjs'
-    },
+    // 输出多种格式
+    output: [
+        {
+            file: 'dist/bundle.amd.js',
+            format: 'amd',
+            sourcemap: false,
+        },
+        {
+            file: 'dist/bundle.cjs.js',
+            format: 'cjs',
+            sourcemap: false,
+        },
+        {
+            file: 'dist/bundle.umd.js',
+            format: 'umd',
+            name: 'example',
+            sourcemap: false,
+        },
+        {
+            file: 'dist/bundle.es.js',
+            format: 'es',
+            sourcemap: false,
+        },
+        {
+            file: 'dist/bundle.iife.js',
+            format: 'iife',
+            name: 'example',
+            sourcemap: false,
+        },
+        {
+            file: 'dist/bundle.system.js',
+            format: 'system',
+            sourcemap: false,
+        },
+    ],
     plugins: [
         typescript(),
     ]
